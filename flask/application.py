a2aa460906789d6e6454090d7c8219bad758847b
Aadarsh Gupta <68748614+infinity1013@users.noreply.github.com>
2021-11-18 04:35:33 +0530

    Update application.py

--- flask/application.py
+++ flask/application.py
@@ -1,6 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,jsonify
+from flask_mail import Mail,Message 
 
 app=Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USE_SSL']=False
+app.config['MAIL_USERNAME']='nemesisunited01@gmail.com'
+app.config['MAIL_PASSWORD']='Demo@123'
+app.config['MAIL_DEFAULT_SENDER']='nemesisunited01@gmail.com'
+mail=Mail(app)
 
 @app.route('/')
 def index():
@@ -34,6 +43,27 @@ def predict():
 @app.route('/appointment')
 def appointment():
 	return render_template('book-appointment.html')
+
+@app.route('/MailMe', methods=['POST', 'GET'])
+def MailMe():
+	fname=request.form.get("app_fname")
+	lname=request.form.get("app_lname")
+	email=request.form.get("app_email")
+	phone=request.form.get("app_phone")
+	department=request.form.get("department_name")
+	date=request.form.get("app_date")
+	symptoms=request.form.get("app_texts")
+	print(department)
+	try:
+		msg=Message(
+			subject='Set Appointment Form',
+			recipients=['nemesisunited01@gmail.com'],
+			body="{} {} wanna set appointment on date {}.\n\n. Here are the details:\n\nEmail: {}\n\nPhone: {}\n\nDepartment:{}\n\n Symptoms:{}\n".format(fname,lname,date,email,phone,department,symptoms)
+		)
+		mail.send(msg)
+		return render_template("index.html",text="Your appointment is scheduled please add date and time to your calendars")
+	except Exception:
+		return render_template("index.html",text="Something went wrong. Please try it later")
 
 if __name__=="__main__":
 	app.run()
